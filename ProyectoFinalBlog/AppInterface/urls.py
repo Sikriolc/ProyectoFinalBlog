bebd4edcb499880679d7cb13dd1ebbdeb8e9bd58
lemolina <lautaro677@gmail.com>
2022-07-03 18:54:39 -0300

    Base vistas CRUD categorias

--- ProyectoFinalBlog/AppInterface/urls.py
+++ ProyectoFinalBlog/AppInterface/urls.py
@@ -5,7 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name="inicio"),
     path('inicio', views.inicio, name="inicio"),
+
     path('categorias', views.categorias, name="categorias"),
+    path('crearcategoria', views.categorias_crear, name="categorias_crear"),
+    path('editarcategoria', views.categorias_editar, name="categorias_editar"),
+
     path('nosotros', views.nosotros, name="nosotros"),
     path('contacto', views.contacto, name="contacto"),
 ]
--- ProyectoFinalBlog/AppInterface/views.py
+++ ProyectoFinalBlog/AppInterface/views.py
@@ -5,13 +5,28 @@ from django.shortcuts import render
 def inicio(request):
     return render(request, "AppInterface/inicio.html",)
 
+#---------------------------------------
+
+
 def categorias(request):
 
   return render(request,"AppInterface/categorias.html",)
 
+def categorias_crear(request):
+
+  return render(request,"AppInterface/categorias_crear.html")
+
+def categorias_editar(request):
+
+  return render(request,"AppInterface/categorias_editar.html")
+
+#---------------------------------------
+
 def nosotros(request):
 
   return render(request,"AppInterface/nosotros.html",)
+
+#---------------------------------------
 
 def contacto(request):
 
