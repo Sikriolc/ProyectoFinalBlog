5852d95e8e2c3ade850b7645a5c730b65f80586b
lemolina <lautaro677@gmail.com>
2022-07-11 16:38:09 -0300

    Modelo de Noticia Listo

--- ProyectoFinalBlog/AppNoticias/models.py
+++ ProyectoFinalBlog/AppNoticias/models.py
@@ -10,16 +10,8 @@ class Noticia(models.Model):
   titulo=models.CharField(max_length=100)
   subtitulo=models.CharField(max_length=100)
   fecha=datetime.now()
-
-  MY_CHOICES2 = (
-                (1, 'Item title 2.1'),
-                (2, 'Item title 2.2'),
-                (3, 'Item title 2.3'),
-                (4, 'Item title 2.4'),
-                (5, 'Item title 2.5')
-                )
-
-  categoria=MultiSelectField(choices = MY_CHOICES2)
+  
+  categoria=models.ForeignKey(Categoria, on_delete=models.CASCADE)
 
   imagen=models.ImageField(upload_to="imagenes/",verbose_name="imagen",null=True)
   cuerpo=models.TextField(null=True)
