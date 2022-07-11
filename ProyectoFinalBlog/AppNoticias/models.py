9da4ecb5838dc7be49c0bfca9dda7719d9f07e2e
lemolina <lautaro677@gmail.com>
2022-07-11 17:58:50 -0300

    Pruebas noticias

--- ProyectoFinalBlog/AppNoticias/models.py
+++ ProyectoFinalBlog/AppNoticias/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 from django.db import models
 from AppInterface.models import *
 from multiselectfield import MultiSelectField
@@ -9,10 +9,8 @@ from multiselectfield import MultiSelectField
 class Noticia(models.Model):
   titulo=models.CharField(max_length=100)
   subtitulo=models.CharField(max_length=100)
-  fecha=datetime.now()
-  
+  fecha=datetime.datetime.now()  
   categoria=models.ForeignKey(Categoria, on_delete=models.CASCADE)
-
   imagen=models.ImageField(upload_to="imagenes/",verbose_name="imagen",null=True)
   cuerpo=models.TextField(null=True)
 
