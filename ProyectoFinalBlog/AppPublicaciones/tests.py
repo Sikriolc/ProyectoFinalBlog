32dc1779989d1d2f6d4f3777cce55cce0277e090
Lautaro Campos <camposemp.20@gmail.com>
2022-07-21 20:07:05 -0300

    test no funciona

--- ProyectoFinalBlog/AppPublicaciones/tests.py
+++ ProyectoFinalBlog/AppPublicaciones/tests.py
@@ -1,3 +1,27 @@
+from email.policy import default
+from urllib import request
 from django.test import TestCase
+from .models import Publicacion
+import datetime as datetime_orig
+
 
 # Create your tests here.
+
+class NoticiasTest(TestCase):
+
+    def Setup(self):
+        Publicacion.objects.create(titulo='Publicacion Test',
+        autor='admin',
+        subtitulo='Esta publicacion es de test',
+        fecha=21/7/2022,
+        hora=datetime_orig.datetime.now(),
+        imagen="",
+        cuerpo='Testeando modelo Publicaciones',)
+
+    def test_publicacion_titulo(self):
+        publicacion = Publicacion.objects.get(titulo='Publicacion Test')
+        self.assertEqual(publicacion.cuerpo, 'Testeando modelo Publicaciones')
+    
+    def test_publicacion_creada(self):
+        publicacion = Publicacion.objects.get(titulo='Publicacion Test')
+        self.assertNotEquals(publicacion, None)
